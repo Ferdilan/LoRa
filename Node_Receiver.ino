ed2c7e0358ea802daf8b5ddc1f77dc6b02109dc1
Ferdilan Ramadhani <90945904+Ferdilan@users.noreply.github.com>
2025-08-16 16:58:38 +0700

    Add files via upload

--- Node_Receiver.ino
+++ Node_Receiver.ino
@@ -13,25 +13,18 @@
 #define OLED_RESET    -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-unsigned long waktuTerima;
-int counter = 0;
-int receivedPackets = 0;
-int totalPackets = 0;
+unsigned long waktuTerima = 0;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
   LoRa.setPins(CS_PIN, RST_PIN, DIO0_PIN);
-
   if (!LoRa.begin(433E6)) {
     Serial.println("Gagal memulai LoRa!");
-    while (1);
+    while (true);
   }
 
-  LoRa.setSpreadingFactor(7); // SF tetap
-  LoRa.setSignalBandwidth(125E3); // 125 kHz BW
-  LoRa.setTxPower(14);  
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("OLED gagal diinisialisasi"));
@@ -46,46 +39,38 @@ void setup() {
   display.display();
 
   Serial.println("LoRa P2P Receiver Siap.");
-  Serial.println("time_ms,paket,rssi_dbm,snr_db"); // header CSV
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    totalPackets++;
 
-    String isiPesan = "";
+    String pesan = "";
     while (LoRa.available()) {
-      isiPesan += (char)LoRa.read();
+      pesan += (char)LoRa.read();
     }
 
     waktuTerima = millis();
     int rssi = LoRa.packetRssi();
     float snr = LoRa.packetSnr();
 
-    // Cetak data dalam format CSV
-    Serial.print(waktuTerima);
-    Serial.print(",");
-    Serial.print(isiPesan);
-    Serial.print(",");
-    Serial.print(rssi);
-    Serial.print(",");
-    Serial.println(snr, 2);
+    Serial.println("=================================");
+    Serial.println("Pesan   : " + pesan);
+    Serial.print("RSSI    : "); Serial.print(rssi); Serial.println(" dBm");
+    Serial.print("SNR     : "); Serial.print(snr, 2); Serial.println(" dB");
+    Serial.print("Waktu   : "); Serial.print(waktuTerima); Serial.println(" ms");
+    Serial.println();
 
-
-     // Cetak ke OLED
     display.clearDisplay();
     display.setCursor(0, 0);
     display.print("Pesan: ");
-    display.println(isiPesan);
+    display.println(pesan);
     display.print("RSSI: ");
     display.print(rssi);
     display.println(" dBm");
     display.print("SNR : ");
     display.print(snr, 1);
     display.println(" dB");
-    display.print("Paket: ");
-    display.println(totalPackets);
     display.display();
   }
 }
